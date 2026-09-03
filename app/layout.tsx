66ab9bb8c19b4c1db02fe0a24932a16c51dd8ba3
hoho601212-collab <hoho601212@gmail.com>
2026-09-03 14:23:07 +0900

    Load regional inquiry form styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css';
 import './insurance.css';
 import './support.css';
+import './inquiry.css';
 import {site} from '@/lib/site';
 import SiteHeader from '@/components/SiteHeader';
 
