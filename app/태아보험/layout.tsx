2bcb2a7914d4acf705d52e70ecd426f748d1d981
hoho601212-collab <hoho601212@gmail.com>
2026-09-07 10:44:44 +0900

    Polish regional information flow and anchor UX

--- app/태아보험/layout.tsx
+++ app/태아보험/layout.tsx
@@ -1,5 +1,6 @@
 import './editorial-standards.css';
 import './support-icons.css';
+import './regional-flow.css';
 import InsuranceEditorialStandards from '@/components/InsuranceEditorialStandards';
 
 export default function InsuranceLayout({children}:{children:React.ReactNode}){
