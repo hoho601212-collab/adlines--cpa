ec9e3a5c11a8f27aed0ec1c9b1e5982c0799379b
hoho601212-collab <hoho601212@gmail.com>
2026-09-16 15:23:47 +0900

    Allow crawler access for fetal insurance landing

--- app/robots.ts
+++ app/robots.ts
@@ -3,7 +3,10 @@ import {site} from '@/lib/site';
 
 export default function robots():MetadataRoute.Robots{
   if(!site.allowIndexing){
-    return {rules:{userAgent:'*',disallow:'/'}};
+    return {
+      rules:{userAgent:'*',allow:'/태아보험',disallow:'/'},
+      sitemap:`${site.baseUrl}/sitemap.xml`
+    };
   }
   return {rules:{userAgent:'*',allow:'/'},sitemap:`${site.baseUrl}/sitemap.xml`};
 }
