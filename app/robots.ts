166691713eb80786ebb1b9e15e2df20b3d2bd8f7
hoho601212-collab <hoho601212@gmail.com>
2026-09-18 11:46:47 +0900

    Make Olbareun Insurance the main site

--- app/robots.ts
+++ app/robots.ts
@@ -2,11 +2,8 @@ import type {MetadataRoute} from 'next';
 import {site} from '@/lib/site';
 
 export default function robots():MetadataRoute.Robots{
-  if(!site.allowIndexing){
-    return {
-      rules:{userAgent:'*',allow:'/태아보험',disallow:'/'},
-      sitemap:`${site.baseUrl}/sitemap.xml`
-    };
-  }
-  return {rules:{userAgent:'*',allow:'/'},sitemap:`${site.baseUrl}/sitemap.xml`};
+  return {
+    rules:{userAgent:'*',allow:'/'},
+    sitemap:`${site.baseUrl}/sitemap.xml`
+  };
 }
